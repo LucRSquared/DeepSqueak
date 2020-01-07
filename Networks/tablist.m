7cfc416a6ec78243b85bf56a177816315df8b958
LucRSquared <LucRSquared@users.noreply.github.com>
2020-01-07 16:18:32 -0600

    Get days

---
+++ Networks/tablist.m
@@ -0,0 +1,11 @@
+% Training with 70% of the data
+data70 = load('DeeperShip_6390T_Tables.mat') ;
+
+days70 = data70.ValidationTables.imageFilename ;
+days70 = cellfun(@(x) x(17:33),days70,'UniformOutput',false) ;
+
+% Training with 80% of the data
+data80 = load('DeepShip_7303T_Tables.mat') ;
+
+days80 = data80.ValidationTables.imageFilename ;
+days80 = cellfun(@(x) x(17:33),days80,'UniformOutput',false) ;
